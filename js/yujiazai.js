edf88a72180445c8f94629e6e2eb1d9d9690b78c
bobiscool <tomisugly@163.com>
2016-10-17 14:56:39 +0800

    微调bug

--- js/yujiazai.js
+++ js/yujiazai.js
@@ -59,11 +59,11 @@ var arr = ['../img/fall.png','../img/jumpinthesea.png'];
 preloadimages(arr);
 
 function preloadimages(arr){
-    var newimages=[]
-    var arr=(typeof arr!="object")? [arr] : arr  //确保参数总是数组
+    var newimages=[];
+    var arr=(typeof arr!="object")? [arr] : arr;  //确保参数总是数组
     for (var i=0; i<arr.length; i++){
         newimages[i]=new Image();
-        newimages[i].src=arr[i]
+        newimages[i].src=arr[i];
         newimages[i].onload = function () {
             console.log('加载完'+this.src)
         }
